52236d39f81eeac532a4a718c8b3991051d56434
Shubhendu Bhakat <kumarsubho2003@gmail.com>
2025-10-22 21:47:58 +0530

    added new build

--- .next/server/app/api/auth/signup/route.js
+++ .next/server/app/api/auth/signup/route.js
@@ -1,8 +1,8 @@
 var R=require("../../../../chunks/[turbopack]_runtime.js")("server/app/api/auth/signup/route.js")
-R.c("server/chunks/[root-of-the-server]__30a5e7d7._.js")
-R.c("server/chunks/node_modules_next_61fa4a4b._.js")
-R.c("server/chunks/node_modules_zod_v4_092dd771._.js")
-R.c("server/chunks/node_modules_bcryptjs_index_42ebb250.js")
-R.m("[project]/.next-internal/server/app/api/auth/signup/route/actions.js [app-rsc] (server actions loader, ecmascript)")
-R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/app/api/auth/signup/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)")
-module.exports=R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/app/api/auth/signup/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)").exports
+R.c("server/chunks/[root-of-the-server]__ef0f07e4._.js")
+R.c("server/chunks/node_modules_next_d739b5ba._.js")
+R.c("server/chunks/[root-of-the-server]__3bb034f3._.js")
+R.c("server/chunks/node_modules_next_dist_esm_build_templates_app-route_bb085be4.js")
+R.m(4826)
+R.m(1064)
+module.exports=R.m(1064).exports
--- .next/static/chunks/app_signin_page_tsx_f95db882._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_7b69962c._.js",
-  "static/chunks/styles_signin_module_ed7b474c.css"
-],
-    source: "dynamic"
-});
